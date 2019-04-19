bb4bd0de5c58ac23fc7361d565ceabc4f8a93a83
Yordan Stoyanov <y.s.stoyanov@hotmail.com>
2019-04-19 15:23:17 +0200

    major changes

--- scripts/animation-home.js
+++ scripts/animation-home.js
@@ -1,1 +1,4 @@
-TweenMax.from("#main", 1.5, {ease:Expo.easeOut, top:800, opacity:0, scale:0, delay:0});
+TweenMax.from("#main", 2, {y:300, opacity:0, ease:Expo.easeOut});
+TweenMax.from("#pr1", 2, {y:500, opacity:0, delay:0.5, ease:Expo.easeOut});
+TweenMax.from("#pr2", 2, {y:500, opacity:0, delay:1, ease:Expo.easeOut});
+TweenMax.from("#pr3", 2, {y:100, opacity:0, delay:1.5, ease:Expo.easeOut});
