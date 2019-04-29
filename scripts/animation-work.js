d7108f83d3b571ed769ca3817168cb1310eff6e2
Yordan Stoyanov <y.s.stoyanov@hotmail.com>
2019-04-29 19:28:06 +0200

    major changes

--- scripts/animation-work.js
+++ scripts/animation-work.js
@@ -2,4 +2,7 @@ TweenMax.from(".intro", 1.5, {y:300, opacity:0, ease:Expo.easeOut});
 TweenMax.from("#pic1", 1.5, {y:200, opacity:0, delay:0.25, ease:Expo.easeOut});
 TweenMax.from("#p2", 1.5, {y:200, opacity:0, delay:0.50, ease:Expo.easeOut});
 TweenMax.from("#images", 1.5, {y:200, opacity:0, delay:0.75, ease:Expo.easeOut});
-TweenMax.from(".next", 1.5, {y:100, opacity:0, delay:1, ease:Expo.easeOut});
+TweenMax.from("#images1", 1.5, {y:200, opacity:0, delay:0.75, ease:Expo.easeOut});
+TweenMax.from("#p3", 1.5, {y:200, opacity:0, delay:1, ease:Expo.easeOut});
+TweenMax.from("#images2", 1.5, {y:200, opacity:0, delay:1.25, ease:Expo.easeOut});
+TweenMax.from(".next", 1.5, {y:100, opacity:0, delay:1.25, ease:Expo.easeOut});
