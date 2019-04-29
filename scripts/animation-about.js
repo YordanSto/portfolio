459a18ca95fc5983a8185abde23a7ca06594b904
Yordan Stoyanov <y.s.stoyanov@hotmail.com>
2019-04-29 22:56:14 +0200

    major changes

---
+++ scripts/animation-about.js
@@ -0,0 +1,4 @@
+TweenMax.from("#yordan1", 1.5, {y:300, opacity:0, ease:Expo.easeOut});
+TweenMax.from("#first", 1.5, {y:200, opacity:0, delay:0.25, ease:Expo.easeOut});
+TweenMax.from("#yordan2", 1.5, {y:200, opacity:0, delay:0.50, ease:Expo.easeOut});
+TweenMax.from("#second", 1.5, {y:200, opacity:0, delay:0.75, ease:Expo.easeOut});
